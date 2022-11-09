29485c9525e081587c4acd679671637f876bae48
Saul Ruiz <saulruiz730@gmail.com>
2022-11-09 14:21:30 -0600

    created get requests for cards

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,8 +1,29 @@
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 
 const CardContainer = (props) => {
+    const [fluttercards, setFlutterCards] = useState([])
+    const [csscards, setCssCards] = useState([])
+    const [pandascards, setPandasCards] = useState([])
+    
+    async function getAllFlutterCards(){
+        let response = await axios.get('http://127.0.0.1:8000/api/collections/1/cards/')
+        console.log(response)
+        setFlutterCards(response.data)
+    }
 
+    async function getAllCssCards(){
+        let response = await axios.get('http://127.0.0.1:8000/api/collections/2/cards/')
+        console.log(response)
+        setCssCards(response.data)
+    }
+
+    async function getAllPandasCards(){
+        let response = await axios.get('http://127.0.0.1:8000/api/collections/3/cards/')
+        console.log(response)
+        setPandasCards(response.data)
+    }
 }
 
 export default CardContainer
